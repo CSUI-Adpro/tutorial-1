c7d8df98864281f285c4d62eb5fe179b91a427e9
eugeniusms <eugeniusms@gmail.com>
2023-02-15 13:22:56 +0700

    #1 MotorCostCalculator Done

--- src/main/java/id/ac/ui/cs/advprog/tutorial1/transport/core/MotorCostCalculator.java
+++ src/main/java/id/ac/ui/cs/advprog/tutorial1/transport/core/MotorCostCalculator.java
@@ -1,33 +1,63 @@
 package id.ac.ui.cs.advprog.tutorial1.transport.core;
 
 
+import id.ac.ui.cs.advprog.tutorial1.transport.exceptions.InvalidDistanceException;
 
 public class MotorCostCalculator implements TransportCostCalculator {
     @Override
     public Cost getCosts(Double distanceInKm) {
-        //TODO: implement method
-        return null;
+        // Melakukan pengecekan jarak sesuai kalkulasi transportasi 0 <= jarak <= 25 km
+        assertWithinDistanceLimit(distanceInKm);
+        // Terdiri dari 3 komponen cost yaitu:
+        // 1) Fare
+        // 2) TimeInHour
+        // 3) Satisfaction
+        Double fare = getTransportFare(distanceInKm);
+        Double timeInHour = getTransportTimeInHour(distanceInKm);
+        Integer satisfaction = getTransportSatisfaction(distanceInKm);
+        // Construct cost baru
+        Cost cost = new Cost(fare, timeInHour, satisfaction);
+        return cost;
     }
     
     @Override
     public Double getTransportFare(Double distanceInKm) {
-        //TODO: implement method
-        return -1.0;
+        // CASE 1: Saat di bawah atau sama dengan 2 km tarif 5000
+        Double fare;
+        if (distanceInKm <= 2) {
+            fare = Double.valueOf(5000);
+            return fare;
+        }
+        // CASE 2: Saat lebih dari 2 km
+        fare = Double.valueOf(5000+(1500*distanceInKm-2));
+        return fare;
     }
     
     @Override
     public Double getTransportTimeInHour(Double distanceInKm) {
-        //TODO: implement method
-        return -1.0;
+        // Kecepatan perjalanan motor adalah 40 km/jam
+        // Waktu = Jarak / Kecepatan
+        Double time = Double.valueOf(distanceInKm/40);
+        return time;
     }
     
     @Override
     public Integer getTransportSatisfaction(Double distanceInKm) {
-        //TODO: implement method
-        return -1;
+        // KM 0-10 Satisfaction tetap 10
+        // KM Selanjutnya -1 / km
+        Integer satisfaction;
+        if (distanceInKm <= 10) {
+            satisfaction = Integer.valueOf(10);
+        }
+        int minPoint = (int) Math.floor(distanceInKm/10);
+        satisfaction = Integer.valueOf(minPoint);
+        return satisfaction;
     }
     
     private void assertWithinDistanceLimit(Double distanceInKm){
-        //TODO: implement method, throws InvalidDistanceException if requirements are not met
+        // Implement method, throws InvalidDistanceException if requirements are not met
+        if (distanceInKm < 0 || distanceInKm > 25) {
+            throw new InvalidDistanceException();
+        }
     }
 }
